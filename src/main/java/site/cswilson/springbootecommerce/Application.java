10b640bf16cf7344b8066eeec82d5615b0ec53fa
transientCW <chris@myubercode.com>
2020-10-17 09:40:18 -0600

    init repo

---
+++ src/main/java/site/cswilson/springbootecommerce/Application.java
@@ -0,0 +1,13 @@
+package site.cswilson.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
